24ee3fbb9dd86873ec45ea7d68dd9964368b151b
bravobryan <120871658+bravobryan@users.noreply.github.com>
2024-06-08 10:28:48 -0700

    Updated linear_regression class object.

--- bravemath/stat_functions/linreg.py
+++ bravemath/stat_functions/linreg.py
@@ -2,8 +2,13 @@ import numpy as np
 from bravemath import stat_functions
 
 
-class LinearRegression:
+class linear_regression:
     def __init__(self, coef=None, intercept=None, model=None):
+        """ Instantiates the linear regression object.
+        Note: Please don't use this for analysis.
+        This object class was created to help solidify my understanding in creating a least squares
+        linear regression model. There are better library packages out there that provide better
+        analysis tools. """
         self.coef = coef
         self.intercept = intercept
         self.model = model
@@ -12,16 +17,20 @@ class LinearRegression:
         r_corr = stat_functions.correlation(x_var, y_var)
         cls.coef = r_corr * (np.std(y_var, ddof=1) / np.std(x_var, ddof=1))
         cls.intercept = np.mean(y_var) - (cls.coef * np.mean(x_var))
-        if cls.intercept < 0:
-            cls.model = f"linearmodel = {cls.coef:.3f}x {cls.intercept:.3f}"
-        else:
-            cls.model = f"linearmodel = {cls.coef:.3f}x + {cls.intercept:.3f}"
+        cls.model = f"y_hat = {cls.intercept:.2f} + {cls.coef:.2f}x"
         return cls
 
 
 if __name__ == '__main__':
+    from sklearn.linear_model import LinearRegression
+    print(help(linear_regression))
     x = [1, 2, 2, 3]
     y = [1, 2, 3, 6]
 
-    linreg = LinearRegression().fit(x, y)
-    print(linreg.model, linreg.coef, linreg.intercept)
+    # -- Code to test if my linear regression algorithm is correct using SciKit Learn's LinearRegression Object class.
+    mylinreg = linear_regression().fit(x, y)
+
+    x = np.reshape(x, (-1, 1))
+    sklinreg = LinearRegression().fit(x, y)
+    print(f"My regression model:            {mylinreg.model}")
+    print(f"Sklearn's regression model:     y_hat = {sklinreg.intercept_:.2f} + {sklinreg.coef_[0]:.2f}x")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from bravemath.stat_functions.linreg import LinearRegression
+from bravemath.stat_functions.linreg import linear_regression
 
 x = [1, 2, 2, 3]
 y = [1, 2, 3, 6]
 
-linreg = LinearRegression().fit(x, y)
+linreg = linear_regression().fit(x, y)
 print(linreg.model)
