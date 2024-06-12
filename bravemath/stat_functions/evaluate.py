24f470dabd011fc55de11c64085376c31a16df7e
bravobryan <120871658+bravobryan@users.noreply.github.com>
2024-06-12 14:09:08 -0700

    updating evaluate.py

--- bravemath/stat_functions/evaluate.py
+++ bravemath/stat_functions/evaluate.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def rmse(y_actual, y_pred, mse=False):
     """
-    Returns Mean Squared Error on input data for testing regression models.
+    Returns Root Mean Squared Error on input data for testing regression models.
     :param y_actual: Array or list of actual response variables.
     :param y_pred: Array or list of predicted response variables.
-    :return: MSE score value. If `squared = True` then RMSE is returned.
+    :return: RMSE score value. If `mse = True` then MSE is returned.
     """
     se = np.square(np.array(y_actual, np.float32) - np.array(y_pred, np.float32))
     mse = (np.sum(se) / (len(se) - 2))
