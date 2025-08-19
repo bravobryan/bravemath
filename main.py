cc59fcd04c3bcf0419b6234701d17712d34c7b3b
bravobryan <120871658+bravobryan@users.noreply.github.com>
2025-08-19 12:28:24 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from bravemath.stat_functions.linreg import linear_regression
-from bravemath.stat_functions.evaluate import rmse
+# from bravemath.stat_functions.linreg import linear_regression
+# from bravemath.stat_functions.evaluate import rmse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 
 
-x = np.array([3.46, 3.36, 1.37, 3.82, 2.75, 0.73, 0.17, 1.20, 2.30, 2.06,
-     2.56, 2.04, 3.70, 2.50, 1.02, 2.11, 3.15, 0.83, 2.81, 0.23])
-y = np.array([2307.23, 2290.87, 23.44, 5285.55, 562.34, 6.44, 1.77, 19.02, 199.53,
-     114.82, 363.08, 131.58, 6014.25, 316.23, 12.57, 128.82, 1412.54, 5.41, 516.52,  1.70])
-y_log = np.log(y)
+number_range = [*range(1, 100)]
+df = pd.DataFrame({
+    "char_col": [f"TheNumber-{i}" for i in number_range],
+    "int_col": [f"  {i}   " for i in range(100, len(number_range)+100)],
+    "random_int": np.random.randint(1,5, size=len(number_range))
+})
 
-linreg = linear_regression().fit(x_var=x, y_var=y)
-linreg.visualize()
+df2 = pd.DataFrame()
+df2['char_num'] = df['char_col'].str.extract('(\d+)').astype(int)
+df2['int_num'] = df['int_col'].str.strip().astype(int)
+df2['letter'] = df['random_int'].replace({1: 'A', 2: 'B', 3: 'C', 4: 'D'})
 
-linreg_log = linear_regression.fit(x, y_log)
-linreg_log.visualize()
+df.to_csv('test.csv', index=False)
+df2.to_csv('test2.csv', index=False)
